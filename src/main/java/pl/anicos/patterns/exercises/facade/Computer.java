101ea6ca625f6b136ed73d63dd2c9758f22f46b9
Adrian Nicos <adrian.nicos@lppsa.com>
2018-02-14 16:24:27 +0100

    adding facade

---
+++ src/main/java/pl/anicos/patterns/exercises/facade/Computer.java
@@ -0,0 +1,19 @@
+package pl.anicos.patterns.exercises.facade;
+
+public class Computer {
+    private CPU cpu;
+    private Memory memory;
+    private HardDrive hardDrive;
+
+    public Computer() {
+        this.cpu = new CPU();
+        this.memory = new Memory();
+        this.hardDrive = new HardDrive();
+    }
+
+    public void run() {
+        cpu.processData();
+        memory.load();
+        hardDrive.readData();
+    }
+}
